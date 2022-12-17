c43f1019ea0db8fa17d866c6ea7b9f8a81e8a57e
PC-imatge-2022-2023 <licenses@enti.cat>
2022-12-17 18:05:16 +0100

    Prefabs, sprites, y box colliders

--- Assets/Scripts/FishControl.cs
+++ Assets/Scripts/FishControl.cs
@@ -5,28 +5,32 @@ using UnityEngine;
 public class FishControl : MonoBehaviour
 {
     Rigidbody2D rb;
+    
     public float speed;
     SpriteRenderer sr;
     public List<Sprite> sprites;
     public FishManager manager;
+    CapsuleCollider2D cc;
     // Start is called before the first frame update
     void Start()
     {
-        manager.actual_fishes++;
-
-        rb = GetComponent<Rigidbody2D>();
-        Vector2 scale = new Vector2(Random.Range(-1f, 1f), 1);
-        scale.Normalize();
-        Debug.Log(scale);
-        Debug.Log(scale);
-
-        sr = GetComponent<SpriteRenderer>();
-        sr.sprite = sprites[Random.Range(0, sprites.Count)];
+        transform.localScale = new Vector2(Random.Range(-0.6f, 0.6f), 0.6f);
+        speed = speed * Random.Range(0.1f, 1f);
     }
 
     // Update is called once per frame
     void Update()
     {
+        manager.actual_fishes++;
 
+        Vector2 direction = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        direction.Normalize();
+        Debug.Log(direction);
+        Debug.Log(direction.magnitude);
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        sr.sprite = sprites[Random.Range(0, sprites.Count)];
+
+        rb.AddForce(direction * speed);
     }
 }
