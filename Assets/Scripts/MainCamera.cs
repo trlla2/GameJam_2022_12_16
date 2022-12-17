6cd1a24cb30018ad9b3c8262b8a15de2adbfaf57
trlla2 <roc.allue@enti.cat>
2022-12-17 12:10:42 +0100

    dios como se mueve la camara

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    public Vector3[] Positions;
+
+    private int mCurrentIndex = 0;
+
+    public float speed = 2.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 currentPos = Positions[mCurrentIndex];
+
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            if (mCurrentIndex < Positions.Length - 1)
+                mCurrentIndex++;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (mCurrentIndex > 0)
+                mCurrentIndex--;
+        }
+        transform.position = Vector3.Lerp(transform.position, currentPos, speed * Time.deltaTime);
+    }
+}
